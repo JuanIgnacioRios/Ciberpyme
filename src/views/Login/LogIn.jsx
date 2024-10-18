a9d44898a14c409739aedfeb7539e339a1163b05
Fede Berro <berro.fede13@gmail.com>
2024-10-18 11:52:45 -0300

    Agregado tailwind

--- src/views/Login/LogIn.jsx
+++ src/views/Login/LogIn.jsx
@@ -1,24 +1,24 @@
-import './LogIn.css';
+import "./LogIn.css";
 
-function LogIn () {
+function LogIn() {
   return (
     <div className="login">
       <div className="login-container">
         <h3>Iniciar sesión</h3>
         <form>
-          <div className='input-container'>
-              <p>Mail</p>
-              <input type="email" placeholder="Email" />
+          <div className="input-container">
+            <p>Mail</p>
+            <input type="email" placeholder="Email" />
           </div>
-          <div className='input-container'>
-              <p>Contraseña</p>
-              <input type="password" placeholder="Password" />
-              <a href=""> ¿Olvidaste tu contraseña?</a>
+          <div className="input-container">
+            <p>Contraseña</p>
+            <input type="password" placeholder="Password" />
+            <a href=""> ¿Olvidaste tu contraseña?</a>
           </div>
           <button type="submit">Sign In</button>
         </form>
-          <p>¿No tenés una cuenta?</p>
-          <a href="">Crear una cuenta</a>
+        <p>¿No tenés una cuenta?</p>
+        <a href="">Crear una cuenta</a>
       </div>
     </div>
   );
